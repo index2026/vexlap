d14914567218b64e3d37e36a75463c081926050c
Hassan Amr <hassan122009pro@gmail.com>
2026-05-06 07:40:30 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // السطر ده مهم جداً لـ GitHub Pages
+  // basePath: '/your-repo-name', // شيل علامتين الـ // واكتب اسم الريبو بتاعك لو مش هتستخدم دومين مخصص
   typescript: {
     ignoreBuildErrors: true,
   },
